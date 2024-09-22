9572a1db31f57fa892f6f853f16662dcf1dfc703
userforfun201 <ngocanhit201@gmail.com>
2024-09-22 13:18:39 +0700

    readme

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace ClickHouseAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.OrderBy(u => u.Timestamp).ToListAsync();
         }
 
         // GET: api/Users/5
